1ffb92bdb00f4db5efb11bb0b311d351ddc87bb4
aajy <aajy.000@gmail.com>
2023-12-26 10:53:56 +0900

    UserInfo.jsx에서  useQuery를 활용한 커스텀 훅useUserQuery생성- 카테고리별로 useQuery를 활용한 커스텀훅 생성시작

--- src/UserInfo.jsx
+++ src/UserInfo.jsx
@@ -1,22 +1,14 @@
-import { useQuery } from '@tanstack/react-query';
-export default function UserInfo() {
-	const fetchUser = async () => {
-		const response = await fetch('https://jsonplaceholder.typicode.com/users');
-		return await response.json();
-	};
+import { useUserQuery } from './hooks/useUsers';
 
-	//useQuery(queryKey, fetch func);
-	// queryKey : react-query로 관리할 각 비동기 데이터 구분용도의 키
-	const { data, isLoading, isSuccess, isError } = useQuery({
-		queryKey: ['users'],
-		queryFn: fetchUser,
-	});
-	//똑같은 데이터인지판별 시 배열로 판별하는 것이 편하므로 배열 형태로.
+export default function UserInfo() {
+	const result = useUserQuery();
+	console.log('result: ', result);
 	return (
 		<div className='UserInfo'>
 			<h1>User Infomation</h1>
-			{isLoading && <p>is loading...</p>}
-			{isSuccess && data.map((user, idx) => <h2 key={user.id}>{user.name}</h2>)}
+			{result.isLoading && <p>is loading...</p>}
+			{result.isSuccess &&
+				result.data.map((user, idx) => <h2 key={user.id}>{user.name}</h2>)}
 		</div>
 	);
 }
